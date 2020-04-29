fd8ac95cb9e995b639e08d70a640ca0231c699d3
akiel <diazorozcoj@gmail.com>
2020-04-29 04:22:17 +0200

    Add tests with pk instead of id

--- subscriptions/tests/test_subscription_confirm.py
+++ subscriptions/tests/test_subscription_confirm.py
@@ -13,7 +13,7 @@ class SubscriptionConfirmTests(APITestCase):
     def test_confirm_subscription_with_existing_token_succeeds(self):
         subscription = Subscription.objects.create(name='name1', email='email@domain.com')
         response = self.client.post(
-            reverse('confirm-subscription', kwargs={'id': subscription.id}))
+            reverse('confirm-subscription', kwargs={'pk': subscription.id}))
         self.assertEqual(response.status_code, status.HTTP_200_OK, 'status code does not match')
         subscription.refresh_from_db()
         self.assertTrue(subscription.is_active, 'subscription is not active')
@@ -22,5 +22,5 @@ class SubscriptionConfirmTests(APITestCase):
 
     def test_confirm_subscription_with_non_existing_token_returns_error(self):
         response = self.client.post(
-            reverse('confirm-subscription', kwargs={'id': 'not really an id'}))
+            reverse('confirm-subscription', kwargs={'pk': 'not really an id'}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, 'status code does not match')
