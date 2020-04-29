3d2748b8c8902413d4608e246e539664e2203007
akiel <diazorozcoj@gmail.com>
2020-04-29 13:57:33 +0200

    Remove not used auth views for drf

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.subscription_create, name='create-subscription'),
     path('<str:pk>/confirm', views.subscription_confirm, name='confirm-subscription'),
     path('<str:pk>', views.subscription_delete, name='delete-subscription'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
