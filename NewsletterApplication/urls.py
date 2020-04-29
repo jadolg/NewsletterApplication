b0c4bfac6d19e06118513c98ed12a8668b6542ba
akiel <diazorozcoj@gmail.com>
2020-04-29 12:18:20 +0200

    Added metrics endpoint

--- NewsletterApplication/urls.py
+++ NewsletterApplication/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subscriptions/', include('subscriptions.urls')),
+    path('', include('django_prometheus.urls')),
 ]
 
 if settings.DEBUG:
