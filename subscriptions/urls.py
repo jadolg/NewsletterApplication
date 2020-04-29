44a106ddb97981b2b93faede5a5ef826930ec297
akiel <diazorozcoj@gmail.com>
2020-04-29 04:19:16 +0200

    Rename id to pk to avoid collision with builtin id

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,7 +4,7 @@ from subscriptions import views
 
 urlpatterns = [
     path('', views.subscription_create, name='create-subscription'),
-    path('<str:id>/confirm', views.subscription_confirm, name='confirm-subscription'),
-    path('<str:id>', views.subscription_delete, name='delete-subscription'),
+    path('<str:pk>/confirm', views.subscription_confirm, name='confirm-subscription'),
+    path('<str:pk>', views.subscription_delete, name='delete-subscription'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -22,11 +22,11 @@ def subscription_create(request):
 
 
 @api_view(['POST', ])
-def subscription_confirm(request, id):
+def subscription_confirm(request, pk):
     """
     Confirms a subscription given an activation token
     """
-    subscription = get_object_or_404(Subscription.objects.all(), id=id)
+    subscription = get_object_or_404(Subscription.objects.all(), id=pk)
     subscription.is_active = True
     subscription.save()
     serializer = SubscriptionSerializer(subscription)
@@ -34,11 +34,11 @@ def subscription_confirm(request, id):
 
 
 @api_view(['DELETE', ])
-def subscription_delete(request, id):
+def subscription_delete(request, pk):
     """
     Deletes a subscription given a deletion token
     """
-    subscription = get_object_or_404(Subscription.objects.all(), id=id)
+    subscription = get_object_or_404(Subscription.objects.all(), id=pk)
     serializer = SubscriptionSerializer(subscription)
     subscription.delete()
     return Response(serializer.data, status=status.HTTP_200_OK)
