4f112b2e9a4050ab8a2aec1ddb2959a26c1f176a
akiel <diazorozcoj@gmail.com>
2020-04-29 04:13:29 +0200

    Add comment on subscription serializer

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -7,6 +7,7 @@ from subscriptions.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     is_active = serializers.BooleanField(read_only=True)
 
+    # Since no email will be really sent when debugging, it is convenient to also have the secret id in the serializer
     if settings.DEBUG:
         id = serializers.UUIDField(read_only=True)
 
